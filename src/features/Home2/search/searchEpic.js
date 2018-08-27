025608a4aef9b09da7d6c76b30154871dc2f1b6e
Nacho Lopez <nacho.lopez@orangex.es>
2018-08-27 11:24:24 +0200

    debounce fixed. now is a switchMap with a delay inside

--- src/features/Home2/search/searchEpic.js
+++ src/features/Home2/search/searchEpic.js
@@ -1,23 +1,25 @@
 import {ofType} from 'redux-observable'
-import { debounceTime, switchMap, map, takeUntil, catchError, retry } from 'rxjs/operators';
-import {fetchApi, fetchUserFail, fetchUserSuccess, clearSearch} from "./searchActions";
+import { debounceTime, switchMap, map, takeUntil, catchError, retry, delay } from 'rxjs/operators';
+import { fetchUserFail, fetchUserSuccess, clearSearch} from "./searchActions";
 import { ajax } from 'rxjs/ajax';
 import { of } from 'rxjs'
 
 export const searchEpic = action$ => action$.pipe(
  
     ofType('FETCH_API'),
-    debounceTime(2000),  //debounce me sirve para que solo mande la ultima (si se invoca en menos de lo que ponga en el debounce), pero imagina que la respuesta tarda mas que ese tiempo en llegar(entre dos peticiones que cumplen el tiempo del debounce). con switchmap la cancelamos
+    switchMap(action => of(action).pipe(
+        delay(2000),
+        takeUntil(action$.ofType('CLEAR_SEARCH'))
+    )),
+    //debounceTime(2000),  //debounce me sirve para que solo mande la ultima (si se invoca en menos de lo que ponga en el debounce), pero imagina que la respuesta tarda mas que ese tiempo en llegar(entre dos peticiones que cumplen el tiempo del debounce). con switchmap la cancelamos
     switchMap(action =>
       ajax.getJSON(`https://api.github.com/search/users?q=${action.payload}+type:user+in:login`).pipe(
-        map(response => fetchUserSuccess(response.items)),
-        retry(2),
-        catchError(error => of({
-          type: 'FETCH_USER_FAIL',
-          payload: error.response.message,
-        })),    
+          retry(2),
+          map(response => fetchUserSuccess(response.items)),
+          catchError(error => of(fetchUserFail(error.response.message))),
           takeUntil(action$.ofType('CLEAR_SEARCH')),
+
       ),
-      
+
     ),
 )
